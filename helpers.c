263ff716305326759031411494fc2fd4d029ae7f
lolocompa <lolocompa@users.noreply.github.com>
2023-09-03 16:17:44 -0300

    confirmación automatizada por check50 [check50=True]

--- helpers.c
+++ helpers.c
@@ -8,9 +8,12 @@ void grayscale(int height, int width, RGBTRIPLE image[height][width])
     {
         for(int j = 0; j < width; j++)
         {
+            int red_value = image[i][j].rgbtRed;
+            int green_value = image[i][j].rgbtGreen;
+            int blue_value = image[i][j].rgbtBlue;
 
-            BYTE avereage = (image[i][j].rgbtRed + image[i][j].rgbtGreen + image[i][j].rgbtBlue)/3.0 + 0x01/2;
-            avereage = round(avereage);
+            int avereage = (red_value + green_value + blue_value)/3 + 0.5;
+
             image[i][j].rgbtRed = avereage;
             image[i][j].rgbtGreen = avereage;
             image[i][j].rgbtBlue = avereage;
