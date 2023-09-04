e70fb86a131adf33b5b5c5366296e1ec73dd4046
lolocompa <lolocompa@users.noreply.github.com>
2023-09-03 21:00:16 -0300

    confirmación automatizada por check50 [check50=True]

--- helpers.c
+++ helpers.c
@@ -75,10 +75,10 @@ void reflect(int height, int width, RGBTRIPLE image[height][width])
 {
     for(int i = 0; i < height; i++)
     {
-        for(int j = 0; j < width/2; j++)
+        for(int j = 0; j < width; j++)
         {
             RGBTRIPLE tmp = image[i][j];
-            image[i][j] = image[i][width - j];
+            image[i][j] = image[i][width - 1 - j];
             image[i][width - j] = tmp;
 
         }
