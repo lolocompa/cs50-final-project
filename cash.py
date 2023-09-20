c5289036d41a746713ee528b38faaec8b9910465
lolocompa <lolocompa@users.noreply.github.com>
2023-09-20 13:01:05 -0300

    confirmación automatizada por check50 [check50=True]

--- cash.py
+++ cash.py
@@ -31,14 +31,15 @@ def get_cents():
         c = cs50.get_float("cents: ")
         if c > 0:
             break
+    c = float(c)
     return c
 
 
 
 
 
-def calculate_quarters(cents):
-    x = cents
+def calculate_quarters(q):
+    x = q
     qua = 0
     while True:
         x = x - 0.25
@@ -49,8 +50,8 @@ def calculate_quarters(cents):
 
 
 
-def calculate_dimes(cents):
-    y = cents
+def calculate_dimes(w):
+    y = w
     dim = 0
     while True:
         y = y - 0.10
@@ -64,8 +65,8 @@ def calculate_dimes(cents):
 
 
 
-def calculate_nickels(cents):
-    z = cents
+def calculate_nickels(e):
+    z = e
     nic = 0
     while True:
         z = z - 0.05
@@ -80,8 +81,8 @@ def calculate_nickels(cents):
 
 
 
-def calculate_pennies(cents):
-    e = cents
+def calculate_pennies(r):
+    e = r
     pen = 0
     while True:
         e = e - 0.01
